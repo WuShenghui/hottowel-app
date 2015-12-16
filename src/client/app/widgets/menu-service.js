47d80e80c446111b9f589820553446ed1cc7588c
WuShenghui <sh.w.sir@outlook.com>
2015-12-16 18:16:34 +0800

    sync menu open/close status when route state changed.

--- src/client/app/widgets/menu-service.js
+++ src/client/app/widgets/menu-service.js
@@ -5,13 +5,14 @@
     .module('app.widgets')
     .factory('menuService', menuService);
 
-  menuService.$inject = ['$location']
+  menuService.$inject = ['$location'];
   function menuService ($location) {
     var service = {
       menuGroups: getMenuGroups(),
       newMenu: newMenu,
       newMenuGroups: newMenuGroups,
       newMenuGroupItem: newMenuGroupItem,
+      refreshMenuStatus: refreshMenuStatus,
       sortByName: sortByName,
       sortByType: sortByType,
       toggleSelectMenuGroup: toggleSelectMenuGroup,
@@ -19,68 +20,81 @@
       selectPage: selectPage
     };
 
-  return service;
+    return service;
 
-  // //////////////
+    // //////////////
 
-  function getMenuGroups () {
-    var menuGroups = [];	
-    
-    return menuGroups;
-  }
-  
-  function newMenu(navRoute) {
-    return newMenuGroupItem(navRoute);
-  }
+    function getMenuGroups () {
+      var menuGroups = [];
 
- function newMenuGroups(navRoute) {
-   return {
-     name: navRoute.settings.menuGroup || navRoute.title,
-     type: 'toggle',
-     menus:[
-         {
-             name: navRoute.title,
-             nav: navRoute.settings.nav,
-             state: navRoute.settings.state,
-             icon: navRoute.settings.icon,
-             type: navRoute.settings.type
-         }
-     ]
-   };
- }
- 
- function newMenuGroupItem(navRoute) {
-   return {
-     name: navRoute.title,
-     nav: navRoute.settings.nav,
-     state: navRoute.settings.state,
-     icon: navRoute.settings.icon,
-     type: navRoute.settings.type
-   };
- }
+      return menuGroups;
+    }
 
-  function toggleSelectMenuGroup (menuGroup) {
-    self.openedMenuGroup = (self.openedMenuGroup === menuGroup ? null : menuGroup);
-  }
+    function newMenu (navRoute) {
+      return newMenuGroupItem(navRoute);
+    }
 
-  function isMenuGroupSelected (menuGroup) {
-    return self.openedMenuGroup === menuGroup;
-  }
+    function newMenuGroups (navRoute) {
+      return {
+        name: navRoute.settings.menuGroup || navRoute.title,
+        type: 'toggle',
+        menus: [
+          {
+            name: navRoute.title,
+            nav: navRoute.settings.nav,
+            state: navRoute.settings.state,
+            icon: navRoute.settings.icon,
+            type: navRoute.settings.type
+          }
+        ]
+      };
+    }
 
-  function selectPage (menuGroup, page) {
-    page && page.url && $location.path(page.url);
-    self.currentMenuGroup = menuGroup;
-    self.currentPage = page;
-  }
+    function newMenuGroupItem (navRoute) {
+      return {
+        name: navRoute.title,
+        nav: navRoute.settings.nav,
+        state: navRoute.settings.state,
+        icon: navRoute.settings.icon,
+        type: navRoute.settings.type
+      };
+    }
 
-  function sortByType (a, b) {
-    return a.type === 'link' || b.type === 'link' ? 1 : 0;
-  }
+    function toggleSelectMenuGroup (menuGroup) {
+      self.openedMenuGroup = menuGroup;
+    }
 
-  function sortByName (a, b) {
-    return (a.name < b.name)  ? -1 :
-      (a.name > b.name) ? 1 : 0;
-  }
+    function isMenuGroupSelected (menuGroup) {
+      return self.openedMenuGroup === menuGroup;
+    }
+
+    function selectPage (menuGroup, page) {
+      page && page.url && $location.path(page.url);
+      self.currentMenuGroup = menuGroup;
+      self.currentPage = page;
+    }
+
+    function refreshMenuStatus (state) {
+      state && state.settings && state.settings.menuGroup;
+
+      for (var index = 0; index < service.menuGroups.length; index++) {
+        if (state.settings.menuGroup == service.menuGroups[index].name) {
+          toggleSelectMenuGroup(service.menuGroups[index]);
+          return;
+        }
+      }
+
+      toggleSelectMenuGroup(null);
+    }
+
+    function sortByType (a, b) {
+      return a.type === 'link' || b.type === 'link' ? 1 : 0;
+    }
+
+    function sortByName (a, b) {
+      return (a.name < b.name) ? -1 :
+        (a.name > b.name) ? 1 : 0;
+    }
 
   }
 })();
